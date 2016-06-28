28207f21828689ec5e3a0eb6f424b6b8a60652e8
JonaMata <jonathanmatarazzi@gmail.com>
2016-06-28 21:31:36 +0200

    Created a window where you can select and add servers (only adds it to serlist.txt)

---
+++ mcServerManager/serverChooser/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace serverChooser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serverListBox = new System.Windows.Forms.ListBox();
+            this.selectedServerLabel = new System.Windows.Forms.Label();
+            this.addServerButton = new System.Windows.Forms.Button();
+            this.addServerTextBox = new wmgCMS.WaterMarkTextBox();
+            this.SuspendLayout();
+            // 
+            // serverListBox
+            // 
+            this.serverListBox.FormattingEnabled = true;
+            this.serverListBox.Location = new System.Drawing.Point(13, 13);
+            this.serverListBox.Name = "serverListBox";
+            this.serverListBox.Size = new System.Drawing.Size(155, 199);
+            this.serverListBox.TabIndex = 2;
+            this.serverListBox.SelectedIndexChanged += new System.EventHandler(this.serverListBox_SelectedValueChanged);
+            // 
+            // selectedServerLabel
+            // 
+            this.selectedServerLabel.AutoSize = true;
+            this.selectedServerLabel.Location = new System.Drawing.Point(175, 13);
+            this.selectedServerLabel.Name = "selectedServerLabel";
+            this.selectedServerLabel.Size = new System.Drawing.Size(0, 13);
+            this.selectedServerLabel.TabIndex = 4;
+            // 
+            // addServerButton
+            // 
+            this.addServerButton.Location = new System.Drawing.Point(197, 226);
+            this.addServerButton.Name = "addServerButton";
+            this.addServerButton.Size = new System.Drawing.Size(75, 23);
+            this.addServerButton.TabIndex = 5;
+            this.addServerButton.Text = "Add Server";
+            this.addServerButton.UseVisualStyleBackColor = true;
+            this.addServerButton.Click += new System.EventHandler(this.addServerButton_Click);
+            // 
+            // addServerTextBox
+            // 
+            this.addServerTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.addServerTextBox.Location = new System.Drawing.Point(13, 229);
+            this.addServerTextBox.Name = "addServerTextBox";
+            this.addServerTextBox.Size = new System.Drawing.Size(155, 20);
+            this.addServerTextBox.TabIndex = 6;
+            this.addServerTextBox.WaterMarkColor = System.Drawing.Color.Gray;
+            this.addServerTextBox.WaterMarkText = "Type server name here...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.addServerTextBox);
+            this.Controls.Add(this.addServerButton);
+            this.Controls.Add(this.selectedServerLabel);
+            this.Controls.Add(this.serverListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox serverListBox;
+        private System.Windows.Forms.Label selectedServerLabel;
+        private System.Windows.Forms.Button addServerButton;
+        private wmgCMS.WaterMarkTextBox addServerTextBox;
+    }
+}
+
---
+++ mcServerManager/serverChooser/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace serverChooser
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<String, String> serverList = new Dictionary<String, String>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            String line;
+            var number = 1;
+            String name;
+            try
+            {
+                StreamReader sr = new StreamReader("serverList.txt");
+                line = sr.ReadLine();
+                number = 1;
+                
+
+                while (line != null)
+                {
+
+                    //create name
+                    name = String.Format(number.ToString());
+                    //check name
+                    if (serverList.ContainsKey(name))
+                    {
+                        serverList[name] = number.ToString();
+                    }
+                    else
+                    {
+                        serverList.Add(name, line);
+                    }
+
+
+                    line = sr.ReadLine();
+                    number += 1;
+
+
+
+
+
+                }
+                sr.Close();
+                line = "Template Server";
+
+
+                //create name
+                name = String.Format(number.ToString());
+                //check name
+                if (serverList.ContainsKey(name))
+                {
+                    serverList[name] = number.ToString();
+                }
+                else
+                {
+                    serverList.Add(name, line);
+                }
+
+
+
+                //Show all dictionary items
+                /*
+                foreach (KeyValuePair<String, String> kvp in serverList)
+                {
+                    Console.WriteLine(String.Format("Key: {0} - Value: {1}", kvp.Key, kvp.Value));
+                }
+                */
+
+                //choose one of the servers and select it's name
+                //Console.WriteLine();
+                //Console.Write("Choose one of the servers above: ");
+                //string server = Console.ReadLine();
+                //Console.WriteLine();
+                //Console.WriteLine("You chose the {0}.", serverList[server]);
+                //Console.ReadLine();
+                serverListBox.DataSource = new BindingSource(serverList, null);
+                serverListBox.DisplayMember = "Value";
+                serverListBox.ValueMember = "Value";
+
+            }
+
+
+
+            finally
+            {
+                selectedServerLabel.Text = null;
+                Console.WriteLine("Executing finally block.");
+            }
+
+
+
+        }
+
+
+
+        private void serverListBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (serverListBox.SelectedIndex != -1)
+            {
+                string selectedServer = serverListBox.SelectedValue.ToString();
+                //string orbit = ListBox1.SelectedValue.ToString();
+                selectedServerLabel.Text = selectedServer;
+            }
+        }
+
+        private void selectServer_Click(object sender, EventArgs e)
+        {
+            string selectedServer = serverListBox.SelectedItem.ToString();
+            selectedServerLabel.Text = selectedServer;
+        }
+
+        private void addServerButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = File.AppendText("serverList.txt");
+                sw.WriteLine();
+                sw.Write(addServerTextBox.Text);
+                sw.Close();
+            }
+            finally
+            {
+                
+                selectedServerLabel.Text = addServerTextBox.Text;
+            }
+        }
+    }
+}
